e1f02679d19b9d68d8bcd14fef5a1fa78e807d56
Fattijenishbek <fattijenishbek01@gmail.com>
2021-01-24 01:03:46 +0600

    added styles and icons to todo list

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def homepage(request):
     return render(request, "index.html")
 
 def test(request):
-    todo_list=ToDo.objects.all()
+    todo_list= ToDo.objects.all()
     return render(request, "test.html", {"todo_list":todo_list})
 
 def third(request):
